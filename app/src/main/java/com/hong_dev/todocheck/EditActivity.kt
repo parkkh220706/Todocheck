d24e463a198b394ce3af13fc95dc51c45036764d
parkkh <parkkh220706@gmail.com>
2023-06-26 01:00:01 +0900

    editactivity 추가중..

--- app/src/main/java/com/hong_dev/todocheck/EditActivity.kt
+++ app/src/main/java/com/hong_dev/todocheck/EditActivity.kt
@@ -2,10 +2,59 @@ package com.hong_dev.todocheck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.hong_dev.todocheck.databinding.ActivityEditBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityEditBinding
+
+    var category:Int = 0
+    var categoryTitle = arrayOf(
+        "ALL", "WORK", "PERSONAL", "STUDY", "ETC", "DONE"
+    )
+    var date: String = "2022년 07월 06일"
+
+    var bottomSheetDialog: BottomSheetDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "할일추가"
+
+        category = intent.getIntExtra("category", 0)
+        binding.tvCategory.text = categoryTitle[category]
+
+        date = SimpleDateFormat("yyyy년 mm월dd일").format(Date())
+        binding.tvDate.text = date
+
+        binding.tvDate.setOnClickListener { showBottomDialogCalendar() }
+        binding.tvCategory.setOnClickListener { showBottomDialogCategory() }
+        binding.btnComplete.setOnClickListener { clickComplete() }
+
+    }
+
+    fun showBottomDialogCalendar(){
+
+    }
+
+    fun showBottomDialogCategory(){
+
+    }
+
+    fun clickCategory(){
+        this.category = category
+        binding.tvCategory.text = categoryTitle[category]
+        bottomSheetDialog?.dismiss()
+    }
+
+    fun clickComplete(){
+
     }
 }
